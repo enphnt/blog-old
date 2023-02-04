c28b15110a3c83b4009cda5f65c57087bf42b018
Nathan Phennel <nathanphennel@gmail.com>
2023-02-04 16:20:19 +1100

    chore: convert Layout to use StaticQuery

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StaticQuery, graphql } from "gatsby"
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import Media from 'react-media';
@@ -8,7 +9,6 @@ import Sidebar from '../components/sidebar';
 import './index.css';
 import './styles/layout-override.css';
 
-
 const title = "Nathan Phennel's Blog";
 
 const bio = (
@@ -17,7 +17,7 @@ const bio = (
       title="Hi, I'm Nathan &nbsp; 👋🏻"
       description={
         <>
-          <img style={{ maxWidth: 190, borderRadius: 19, padding: 10, marginBottom: 0 }} src={`images/profile-pic.png`} />
+          <img style={{ maxWidth: 190, borderRadius: 19, padding: 10, marginBottom: 0 }} src={`images/profile-pic.png`} alt="profile-pic" />
           <div style={{ padding: "0 10px" }}>
             <p>I use this space for writing down things I want to remember for future reference.</p>
             <strong>Who am I?</strong>
@@ -29,64 +29,78 @@ const bio = (
 )
 
 const Layout = ({ children }) => (
-  <div>
-    <Helmet
-      title={title}
-      meta={[
-        { name: 'description', content: title },
-        { name: 'keywords', content: 'software testing, software development, javascript, software engineer in test' },
-      ]}
-    />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 1500,
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        height: '100%',
-      }}
-    >
-      <Media query={{ maxWidth: 848 }}>
-        {matches =>
-          matches ? (
-            <div
-              style={{
-                margin: '0 auto',
-                maxWidth: 980,
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'space-between',
-                height: '100%',
-                padding: '0 25px',
-              }}
-            >
-              <div style={{ flex: 1 }}>{children}</div>
-              {bio}
-            </div>
-          ) : (
-            <div
-              style={{
-                margin: '0 auto',
-                maxWidth: 1500,
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                height: '100%',
-                padding: '0 25px 75px 25px',
-              }}
-            >
-              <div style={{ flex: 2.5, paddingRight: '30px' }}>
-                {children}
-              </div>
-              {bio}
-            </div>
-          )
-        }
-      </Media>
-    </div>
-  </div>
+  <StaticQuery
+    render={data =>
+      <>
+        <Helmet
+          title={title}
+          meta={[
+            { name: 'description', content: title },
+            { name: 'keywords', content: 'software testing, software development, javascript, software engineer in test' },
+          ]}
+        />
+        <Header />
+        <div
+          style={{
+            margin: '0 auto',
+            maxWidth: 1500,
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            height: '100%',
+          }}
+        >
+          <Media query={{ maxWidth: 848 }}>
+            {matches =>
+              matches ? (
+                <div
+                  style={{
+                    margin: '0 auto',
+                    maxWidth: 980,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'space-between',
+                    height: '100%',
+                    padding: '0 25px',
+                  }}
+                >
+                  <div style={{ flex: 1 }}>{children}</div>
+                  {bio}
+                </div>
+              ) : (
+                <div
+                  style={{
+                    margin: '0 auto',
+                    maxWidth: 1500,
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    height: '100%',
+                    padding: '0 25px 75px 25px',
+                  }}
+                >
+                  <div style={{ flex: 2.5, paddingRight: '30px' }}>
+                    {children}
+                  </div>
+                  {bio}
+                </div>
+              )
+            }
+          </Media>
+        </div>
+      </>
+    }
+    query={
+      graphql`
+        query LayoutQuery {
+          site {
+            siteMetadata {
+              title
+            }
+          }
+        }`
+    }
+  />
 );
 
 Layout.propTypes = {
@@ -94,13 +108,3 @@ Layout.propTypes = {
 };
 
 export default Layout;
-
-export const query = graphql`
-  query SiteTitleQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`;
