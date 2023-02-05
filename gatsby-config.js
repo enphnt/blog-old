2f99b4925dd876cc0028c102540abdda4ad01463
Nathan Phennel <nathanphennel@gmail.com>
2023-02-05 13:32:36 +1100

    feat: robots.txt and sitemap.xml generation

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,10 @@
 module.exports = {
   pathPrefix: `/blog`,
   siteMetadata: {
-    title: 'Nathan Phennel Blog',
+    title: "Software, music, innovations and other things worth knowing about. | Nathan Phennel's Blog",
+    description: "Notes and memos on the things that cross my path.",
     siteUrl: `https://enphnt.github.io/blog/`,
+    image: "/images/profile-pic.png"
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -25,5 +27,62 @@ module.exports = {
       },
     },
     `gatsby-transformer-remark`,
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: 'https://enphnt.github.io/blog', // truncated in the robots.txt result. not sure if it'll work.
+        sitemap: 'https://enphnt.github.io/blog/sitemap/sitemap-0.xml',
+        policy: [{ userAgent: '*', allow: '/' }],
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-sitemap',
+      options: {
+        excludes: ['/page-2', '/confirmed'],
+        query: `
+          {
+            site {
+              siteMetadata {
+                siteUrl
+              }
+            }
+            allSitePage {
+              nodes {
+                path
+              }
+            }
+            allMarkdownRemark {
+              nodes {
+                frontmatter {
+                  path
+                  date
+                }
+              }
+            }
+          }`,
+        resolveSiteUrl: ({ site: { siteMetadata } }) => {
+          return siteMetadata.siteUrl + "blog";
+        },
+        serialize: ({ path }) => {
+          const result = {
+            url: `blog${path}`,
+            changefreq: "monthly",
+          }
+
+          // mark homepage as higher priority
+          if (path === "/") {
+            return {
+              ...result,
+              priority: 0.7,
+            }
+          }
+
+          return {
+            ...result,
+            priority: 0.5,
+          }
+        }
+      },
+    }
   ],
 }
