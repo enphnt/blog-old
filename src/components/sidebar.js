474fc96a85c07ab271948de47f6178876181dbb9
Nathan Phennel <nathanphennel@gmail.com>
2023-02-19 19:03:28 +1100

    content(sidebar): simplify sidebar bio info

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -4,12 +4,14 @@ const Sidebar = props => (
   <div style={{ flex: .66 }}>
     <div className="sidebar">
       <strong style={{ textAlign: "center" }}>Hi, I'm Nathan &nbsp; 👋🏻</strong>
-      <img style={{ height: 190, width: 190, objectFit: "contain", borderRadius: 19, padding: 10, marginBottom: 0 }} src={`images/profile-pic.png`} alt="profile-pic" />
-      <div style={{ padding: "0 10px" }}>
-        <p style={{ fontSize: 13 }}>I use this space for writing down things I want to remember for future reference.</p>
-        <strong style={{ fontSize: 13 }}>Who am I?</strong>
-        <p style={{ fontSize: 13 }}>From 9-5, I develop software but after hours I'm playing guitar, drums or keys as a DIY producer. I'm always learning about random things and some end up here.</p>
-      </div>
+      <img
+        style={{ height: 190, width: 190, objectFit: "contain", borderRadius: 19, padding: 10, marginBottom: 0 }}
+        src={`images/profile-pic.png`}
+        alt="profile-pic" />
+      <p style={{ fontSize: 13 }}>
+        From 9-5, I develop software, but after hours I'm playing the guitar and drums, or reading something new. I love to
+        learn about random things, and some of the nuggets I discover end up here.
+      </p>
     </div>
   </div>
 );
