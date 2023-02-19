7357ace4c7589d530931a91f8c9dba8e945bfbac
Nathan Phennel <nathanphennel@gmail.com>
2023-02-19 20:45:53 +1100

    feat: toc's via gatsby-remark-table-of-contents

--- gatsby-config.js
+++ gatsby-config.js
@@ -95,6 +95,26 @@ module.exports = {
           }
         }
       },
-    }
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-table-of-contents`,
+            options: {
+              exclude: "Table of Contents",
+              tight: false,
+              ordered: false,
+              fromHeading: 1,
+              toHeading: 6,
+              className: "table-of-contents"
+            },
+          },
+          `gatsby-remark-autolink-headers`
+        ],
+      },
+    },
+    `gatsby-remark-autolink-headers`
   ],
 }
